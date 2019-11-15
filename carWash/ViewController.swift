f425d2c0449fe821a5f454682bb262cfbecea5c5
Juliett13 <k.julia_13@mail.ru>
2019-11-15 17:15:48 +0300

    Initial Commit

---
+++ carWash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  carWash
+//
+//  Created by Juliett Kuroyan on 15.11.2019.
+//  Copyright © 2019 VooDooLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
